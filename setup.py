26236e85627fa83475242a2abaaccd4b48e13054
Mitch Garnaat <mitch@garnaat.com>
2016-01-11 00:35:13 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requires = [
 
 setup(
     name='cruddy',
-    version='0.4.0',
+    version='0.4.1',
     description='A CRUD wrapper class for Amazon DynamoDB',
     long_description=open('README.md').read(),
     author='Mitch Garnaat',
